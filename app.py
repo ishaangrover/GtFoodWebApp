64a992fb3fda39844deeae645ff6d04093fbf2d5
ishaan <ishaangrover@gatech.edu>
2016-04-26 10:51:46 -0400

    added more changes

--- app.py
+++ app.py
@@ -46,24 +46,30 @@ def authenticate_stripe(temp):
 @socketio.on('charge')
 def charge(data):
 	global map_cid_amount_chargeid
+	print 'IN CHARGE'
+	data = ast.literal_eval(json.dumps(data))
 	c_id = data["c_id"]
 	total = data["amount"]
+	print 'c_id {} Total {}'.format(c_id, total)
 	if total:
+		print 'About to charge something'
 		total = int(total*100)
 		charge = stripe.Charge.create(
 					amount=total, # in cents
 					currency="usd",
 					customer=c_id
 				)
+		print 'Charge created: {}'.format(charge.id)
 		map_cid_amount_chargeid[c_id] = (total, charge.id)
 
 
 @socketio.on('refund')
 def refund(customer_id):
-	amount, charge_id = map_cid_amount_chargeid[customer_id]
+	cust_id = customer_id.encode('ascii', 'ignore')
+	amount, charge_id = map_cid_amount_chargeid[cust_id]
 	print 'charge id: {}'.format(charge_id)
 	re = stripe.Refund.create(charge=charge_id)
-	del map_cid_amount_chargeid[customer_id]
+	del map_cid_amount_chargeid[cust_id]
 
 
 @app.route('/order_deliver/<token>')
@@ -94,6 +100,13 @@ def pending_orders(token):
 	else:
 		return "IN waiting"
 
+@app.route('/in_progress/<token>/<orderer_uid>')
+def in_progress(token, orderer_uid):
+	if token in authenticated_tokens:
+		return render_template("inProgress.html")
+	else:
+		return "IN Progress"
+
 @app.route('/deliveryOnWay/<token>')
 def delivery_on_way(token):
 	if token in authenticated_tokens:
@@ -107,6 +120,21 @@ def stripe_deliverer(token):
 		return render_template("stripe_deliverer.html")
 	else:
 		return "IN STRIPE DELIVERER"
+
+@app.route('/order_complete/<token>')
+def order_complete(token):
+	if token in authenticated_tokens:
+		return render_template("orderComplete.html")
+	else:
+		return "IN order complete"
+
+@app.route('/delivery_complete/<token>')
+def delivery_complete(token):
+	if token in authenticated_tokens:
+		return render_template("delivery_complete.html")
+	else:
+		return "IN delivery complete"
+
 
 @app.route('/menu/<token>')
 def menu(token):
